47da7fe8ae0ae6050651e401dc7f1229086e323a
Maroua Jalil <mjalil@SMB.SQLI.COM>
2021-03-01 15:39:11 +0100

    second commit

--- src/com/sqli/Main.java
+++ src/com/sqli/Main.java
@@ -3,7 +3,7 @@ package com.sqli;
 public class Main {
 
     public static void main(String[] args) {
-        Systeme.out.println("first Modifiation");
-
+        System.out.println("first Modifiation");
+        System.out.println("*****************");
     }
 }
